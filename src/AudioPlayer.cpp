3b041dcc84527a61c2b850411abad12093a73757
case-steamer <hipster1911@gmail.com>
2026-05-02 12:01:18 -0500

    handle thread scheduling so that the AudioPlayer does not shut down until the track is finished playing.

--- src/AudioPlayer.cpp
+++ src/AudioPlayer.cpp
@@ -8,13 +8,21 @@
 void AudioPlayer::playTrack(const fs::path& filepath)
 {
     result = ma_engine_init(NULL, &engine);
+    ma_sound sound;
+    ma_sound_init_from_file(&engine, filepath.c_str(), 0, NULL, NULL, &sound);
+
     if (result != MA_SUCCESS)
     {
         std::cout << "Audio failure" << std::endl;
         return;
     }
 
-    ma_engine_play_sound(&engine, filepath.c_str(), NULL);
-    std::this_thread::sleep_for(std::chrono::seconds(5));
+                ma_sound_start(&sound);
+    float sTime;
+    ma_sound_get_length_in_seconds(&sound, &sTime);
+    std::this_thread::sleep_for(std::chrono::milliseconds((int)(sTime * 1000)));
+
+
+    ma_sound_uninit(&sound);
     ma_engine_uninit(&engine);
 }
