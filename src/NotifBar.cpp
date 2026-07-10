aab1143318fa89eabee59b7cb9467f6c240ebfc7
James Haining <hipster1911@gmail.com>
2026-07-10 12:34:34 -0500

    build out NotifBar::scanMessages()

--- src/NotifBar.cpp
+++ src/NotifBar.cpp
@@ -1,8 +1,10 @@
 #include <string>
+#include <chrono>
 
 #include "imgui.h"
 
 #include "NotifBar.h"
+#include "SpecialStructs.h"
 
 NotifBar::NotifBar(Driver& driver) : driver(driver)
 {
@@ -11,5 +13,22 @@ NotifBar::NotifBar(Driver& driver) : driver(driver)
 
 void NotifBar::render()
 {
-
+    scanMessages();
 }
+
+void NotifBar::scanMessages()
+{
+    if (!driver.messageDeck.empty())
+    {
+        for (msgData md : driver.messageDeck)
+        {
+            ImGui::TextUnformatted(md.message.c_str());
+        }
+    
+        if ((std::chrono::steady_clock::now() - driver.messageDeck.front().timestamp) > std::chrono::seconds(5))
+        {
+            driver.messageDeck.pop_front();
+        }
+    }
+}
+
